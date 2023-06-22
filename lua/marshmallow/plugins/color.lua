5e08b9c335bff559a92003e0d74260d9848248e4
marshmallow <marshycity@gmail.com>
2023-06-22 20:55:38 +1000

    add ts playground

--- lua/marshmallow/plugins/color.lua
+++ lua/marshmallow/plugins/color.lua
@@ -10,6 +10,7 @@ return {
 					mode = "topline",
 				},
 			},
+			"nvim-treesitter/playground",
 		},
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
@@ -32,6 +33,7 @@ return {
 					"tsx",
 					"javascript",
 					"css",
+					"org",
 				},
 				highlight = {
 					enable = true,
@@ -79,6 +81,24 @@ return {
 						swap_previous = {
 							["<leader>,"] = "@parameter.inner",
 						},
+					},
+				},
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+					persist_queries = false, -- Whether the query persists across vim sessions
+					keybindings = {
+						toggle_query_editor = "o",
+						toggle_hl_groups = "i",
+						toggle_injected_languages = "t",
+						toggle_anonymous_nodes = "a",
+						toggle_language_display = "I",
+						focus_language = "f",
+						unfocus_language = "F",
+						update = "R",
+						goto_node = "<cr>",
+						show_help = "?",
 					},
 				},
 			})
