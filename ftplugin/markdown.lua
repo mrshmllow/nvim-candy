5e0624ec8310175665552c62afcb247384b60e34
marshmallow <marshycity@gmail.com>
2023-06-22 20:55:04 +1000

    update ftplugins

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,3 +1,5 @@
 vim.b.minicursorword_disable = true
 vim.o.textwidth = 80
 vim.o.spell = true
+vim.o.signcolumn = "no"
+vim.b.show_word_count = true
--- ftplugin/norg.lua
+++
@@ -1,3 +0,0 @@
-vim.b.minicursorword_disable = true
-vim.o.textwidth = 80
-vim.o.spell = true
