eb2d21e3943b81ab12ad377f0c126b7d05c3cbbc
marshmallow <marshycity@gmail.com>
2023-06-24 14:21:32 +1000

    remove legacy inlay hint stuff

--- lua/marshmallow/au.lua
+++ lua/marshmallow/au.lua
@@ -4,5 +4,3 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		vim.o.filetype = "markdown"
 	end,
 })
-
--- vim.cmd("autocmd BufEnter,InsertLeave,BufWritePost <buffer> lua vim.lsp.inlay_hint.refresh()")
