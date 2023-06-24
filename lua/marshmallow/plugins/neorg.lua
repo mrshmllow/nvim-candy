3e308b123030bfb9deeab2804a770e38087756e6
marshmallow <marshycity@gmail.com>
2023-06-24 15:00:58 +1000

    remove neorg

--- lua/marshmallow/plugins/neorg.lua
+++ lua/marshmallow/plugins/neorg.lua
@@ -1,42 +1,5 @@
 return {
 	{
-		"nvim-neorg/neorg",
-		build = ":Neorg sync-parsers",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			-- "folke/zen-mode.nvim"
-		},
-		config = function()
-			require("neorg").setup({
-				load = {
-					["core.defaults"] = {},
-					["core.concealer"] = {},
-					["core.dirman"] = {
-						config = {
-							workspaces = {
-								notes = "~/notes",
-							},
-						},
-					},
-					["core.clipboard.code-blocks"] = {},
-					["core.export"] = {},
-					["core.export.markdown"] = {
-						config = {
-							extensions = "all",
-						},
-					},
-					["core.completion"] = {
-						config = { engine = "nvim-cmp" },
-					},
-					["core.summary"] = {},
-					-- ["core.presenter"] = {
-					-- 	config = { zen_mode = "zen-mode" },
-					-- },
-				},
-			})
-		end,
-	},
-	{
 		"dhruvasagar/vim-table-mode",
 		lazy = false,
 	},
