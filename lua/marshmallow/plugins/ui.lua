c94fb125740f52037707a74257fcf738897e012e
marshmallow <marshycity@gmail.com>
2023-06-24 14:05:33 +1000

    use mini.files

--- lua/marshmallow/plugins/ui.lua
+++ lua/marshmallow/plugins/ui.lua
@@ -841,26 +841,88 @@ return {
 			},
 		},
 	},
+	-- {
+	-- 	"stevearc/oil.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 	},
+	-- 	opts = {
+	-- 		columns = {
+	-- 			"icon",
+	-- 		},
+	-- 		trash_command = "gio trash",
+	-- 		delete_to_trash = true,
+	-- 		keymaps = {
+	-- 			["g?"] = "actions.show_help",
+	-- 			["<CR>"] = "actions.select",
+	-- 			["<C-v>"] = "actions.select_vsplit",
+	-- 			["<C-s>"] = false,
+	-- 			["<C-h>"] = false,
+	-- 			["<C-t>"] = "actions.select_tab",
+	-- 			["<C-p>"] = "actions.preview",
+	-- 			["<C-c>"] = false,
+	-- 			["<C-l>"] = false,
+	-- 			["-"] = "actions.parent",
+	-- 			["_"] = "actions.open_cwd",
+	-- 			["`"] = "actions.cd",
+	-- 			["~"] = "actions.tcd",
+	-- 			["g."] = "actions.toggle_hidden",
+	-- 		},
+	-- 	},
+	-- 	keys = {
+	-- 		{
+	-- 			"-",
+	-- 			function()
+	-- 				require("oil").open()
+	-- 			end,
+	-- 			desc = "Open parent directory",
+	-- 		},
+	-- 	},
+	-- },
 	{
-		"stevearc/oil.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
+		"echasnovski/mini.files",
+		version = false,
+		lazy = false,
 		opts = {
-			columns = {
-				"icon",
+			options = {
+				use_as_default_explorer = true,
 			},
 		},
 		keys = {
 			{
 				"-",
 				function()
-					require("oil").open()
+					require("mini.files").open()
 				end,
-				desc = "Open parent directory",
 			},
 		},
-		lazy = false,
+		config = function(_, opts)
+			require("mini.files").setup(opts)
+
+			local show_dotfiles = true
+
+			local filter_show = function(fs_entry)
+				return true
+			end
+
+			local filter_hide = function(fs_entry)
+				return not vim.startswith(fs_entry.name, ".")
+			end
+
+			local toggle_dotfiles = function()
+				show_dotfiles = not show_dotfiles
+				local new_filter = show_dotfiles and filter_show or filter_hide
+				require("mini.files").refresh({ content = { filter = new_filter } })
+			end
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					local buf_id = args.data.buf_id
+					vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+				end,
+			})
+		end,
 	},
 	{
 		"tpope/vim-dadbod",
