32c8a50bca0e45796beca4ae7546d2e964692947
marshmallow <marshycity@gmail.com>
2023-07-07 15:39:58 +1000

    buf write pre format

--- lua/marshmallow/au.lua
+++ lua/marshmallow/au.lua
@@ -1,12 +1,27 @@
+local id = vim.api.nvim_create_augroup("Marshmallow", {
+	clear = true,
+})
+
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = "*.mdx",
+	group = id,
 	callback = function()
 		vim.o.filetype = "markdown"
 	end,
 })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
+	group = id,
 	callback = function()
 		vim.cmd("wincmd =")
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = id,
+	callback = function()
+		vim.lsp.buf.format({
+			async = false,
+		})
 	end,
 })
