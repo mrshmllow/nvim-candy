5e3bb53a571e4f71c613c6a3f2f57daf06d05af2
marshmallow <marshycity@gmail.com>
2024-01-25 23:42:35 +1100

    remove firenvim
    i never use it

--- lua/marshmallow/plugins/more.lua
+++ lua/marshmallow/plugins/more.lua
@@ -1,14 +1,5 @@
 return {
 	{
-		"glacambre/firenvim",
-		cond = not not vim.g.started_by_firenvim,
-		build = function()
-			require("lazy").load({ plugins = "firenvim", wait = true })
-			vim.fn["firenvim#install"](0)
-		end,
-	},
-
-	{
 		"mrshmllow/open-handlers.nvim",
 		cond = vim.ui.open ~= nil,
 		lazy = false,
