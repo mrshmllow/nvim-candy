4979a98e9658b8ea95c23af94601d238dccb4018
marshmallow <marshycity@gmail.com>
2024-02-26 19:26:29 +1100

    add extra highlights

--- candy/lua/marshmallow/colorscheme.lua
+++ candy/lua/marshmallow/colorscheme.lua
@@ -9,7 +9,20 @@ require("catppuccin").setup({
 			MiniMapDiagnosticWarn = { bg = colors.yellow, fg = colors.yellow },
 			MiniMapDiagnosticInfo = { bg = colors.teal, fg = colors.teal },
 			MiniMapDiagnosticHint = {},
+
+			MiniFilesBorder = { fg = colors.surface0 },
+			MiniFilesNormal = { bg = colors.base },
+
+			MiniPickBorder = { fg = colors.surface0 },
+			MiniPickNormal = { bg = colors.base },
+			MiniPickPrompt = { fg = colors.mauve },
+
+			FloatBorder = { fg = colors.mauve },
+
 			Directory = { fg = colors.mauve },
+
+			-- native_lsp method doesnt work?
+			LspInlayHint = { fg = colors.overlay0, bg = colors.base },
 		}
 	end,
 })
