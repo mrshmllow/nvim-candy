f2d9aff366ff3f0408ceeaafc26497d72a3b1456
marshmallow <marshycity@gmail.com>
2024-05-06 21:13:28 +1000

    add wsl clipboard

---
+++ candy/lua/marshmallow/env/wsl.lua
@@ -0,0 +1,12 @@
+vim.g.clipboard = {
+	name = "WslClipboard",
+	copy = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe",
+	},
+	paste = {
+		["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+	},
+	cache_enabled = 0,
+}
