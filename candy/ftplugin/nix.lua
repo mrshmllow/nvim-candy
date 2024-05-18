abace16ab6c671802db2edee007114a98b520eaf
marshmallow <marshycity@gmail.com>
2024-05-19 00:08:56 +1000

    add ftplugin/nix.lua

---
+++ candy/ftplugin/nix.lua
@@ -0,0 +1,10 @@
+-- Be more in-line with alejandra
+
+-- Tabs / Spaces --
+local tab_width = 4
+
+vim.opt.expandtab = true
+
+vim.opt.tabstop = tab_width
+vim.opt.shiftwidth = tab_width
+vim.opt.softtabstop = tab_width
