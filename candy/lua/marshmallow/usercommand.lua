aae490aa7cbe57325e8c3733ccc494c743bea0f7
marshmallow <marshycity@gmail.com>
2024-05-28 20:23:37 +1000

    add :Z usercommand

--- candy/lua/marshmallow/usercommand.lua
+++ candy/lua/marshmallow/usercommand.lua
@@ -5,3 +5,15 @@ vim.api.nvim_create_user_command("Trans", function()
 		vim.g.neovide_transparency = 0.9
 	end
 end, {})
+
+vim.api.nvim_create_user_command("Z", function(val)
+	local obj = vim.system({ "fish", "-c", "z -e " .. val["args"] }, { text = true }):wait()
+
+	if obj.code == 0 then
+		return vim.cmd.cd(obj.stdout:sub(1, -2)) -- remove \n
+	end
+
+	vim.notify("Error: " .. obj.stderr, vim.log.levels.ERROR)
+end, {
+	nargs = 1,
+})
