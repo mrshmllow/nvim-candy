3188afab4bd5ce34c57ec02aef1a9b2e85fcbc74
marshmallow <marshycity@gmail.com>
2024-06-22 12:10:30 +1000

    only require wsl if under wsl

--- candy/lua/marshmallow/env/init.lua
+++ candy/lua/marshmallow/env/init.lua
@@ -1,2 +1,6 @@
-require("marshmallow.env.wsl")
+-- prob a better way to do this
+if string.match(vim.uv.os_uname().release, "WSL") ~= nil then
+	require("marshmallow.env.wsl")
+end
+
 require("marshmallow.env.gui")
