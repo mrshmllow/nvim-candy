f84224cf792fa7572577e9c595b6ed7e736f7d36
marshmallow <marshycity@gmail.com>
2024-06-22 11:57:03 +1000

    Add git extmarks to mini.files

--- candy/lua/marshmallow/files.lua
+++ candy/lua/marshmallow/files.lua
@@ -12,6 +12,8 @@ local id = vim.api.nvim_create_augroup("marshmallow-mini-files", {
 	clear = true,
 })
 
+local ns = vim.api.nvim_create_namespace("marshmallow-mini-files")
+
 local show_dotfiles = true
 
 local filter_show = function()
@@ -34,5 +36,56 @@ vim.api.nvim_create_autocmd("User", {
 	callback = function(args)
 		local buf_id = args.data.buf_id
 		vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle dotfiles" })
+	end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferUpdate",
+	group = id,
+	callback = function(args)
+		local data = args.data
+		local buf_id = data.buf_id
+		local lines = vim.api.nvim_buf_get_lines(data.buf_id, 0, -1, false)
+
+		for line_num in ipairs(lines) do
+			local fs_entry = require("mini.files").get_fs_entry(buf_id, line_num)
+
+			if fs_entry == nil then
+				goto continue
+			end
+
+			local obj = vim.system({ "git", "diff", "--exit-code", "--shortstat", fs_entry.path }, { text = true })
+				:wait()
+
+			if obj.code ~= 1 then
+				goto continue
+			end
+
+			local changes = {}
+
+			if fs_entry.fs_type == "directory" then
+				-- first number in the string is the number of files changed
+				table.insert(changes, {
+					"~" .. string.match(obj.stdout, "%d"),
+					"GitSignsChange",
+				})
+			end
+
+			for value in string.gmatch(obj.stdout, "%d+ %w+%([-+]%)") do
+				local number = string.match(value, "%d+")
+				local change = string.match(value, "[-+]")
+
+				table.insert(changes, {
+					change .. number,
+					change == "-" and "GitSignsDelete" or "GitSignsAdd",
+				})
+			end
+
+			vim.api.nvim_buf_set_extmark(buf_id, ns, line_num - 1, 0, {
+				virt_text = changes,
+			})
+
+			::continue::
+		end
 	end,
 })
