aa49e7fe4fd96fee25c5c94e1859072fbded731d
marshmallow <marshycity@gmail.com>
2024-10-01 10:16:38 +1000

    merge neovide seperated keymap

--- candy/lua/marshmallow/remap.lua
+++ candy/lua/marshmallow/remap.lua
@@ -132,8 +132,7 @@ if vim.g.neovide then
 	end)
 
 	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set({ "n", "v" }, "<D-v>", '"+P') -- Paste
 	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
 	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
