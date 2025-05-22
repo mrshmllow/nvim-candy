426910c5581666fb1724b69cbb82ce57c6a71347
marshmallow <marshycity@gmail.com>
2025-05-23 09:36:57 +1000

    add structure joining lines in rust

--- candy/ftplugin/rust.lua
+++ candy/ftplugin/rust.lua
@@ -16,3 +16,15 @@ end, { buffer = true, silent = true })
 vim.keymap.set("n", "K", function()
 	vim.cmd.RustLsp({ "hover", "actions" })
 end, { buffer = true, silent = true })
+
+vim.keymap.set("n", "J", function()
+	if vim.v.count >= 1 then
+		vim.cmd(".,.+" .. vim.v.count - 1 .. "join")
+	else
+		vim.cmd.RustLsp("joinLines")
+	end
+end, { buffer = true, silent = true })
+
+vim.keymap.set("v", "J", function()
+	vim.cmd.RustLsp("joinLines")
+end, { buffer = true, silent = true })
