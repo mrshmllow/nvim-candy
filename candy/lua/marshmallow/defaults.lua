e2a3ef3b2835e642d778d3d8d730f4f6ce012f67
marshmallow <marshycity@gmail.com>
2025-05-22 13:40:19 +1000

    remove diagnostic icons i dont really use

--- candy/lua/marshmallow/defaults.lua
+++ candy/lua/marshmallow/defaults.lua
@@ -70,13 +70,7 @@ vim.opt.foldenable = false
 -- Is this okay??
 vim.opt.linespace = -2
 
--- Diagnostics --
--- I use nf-fa nerd icons
-vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
-
+-- Directory stuff --
 vim.opt.exrc = true
 
 -- Completion --
